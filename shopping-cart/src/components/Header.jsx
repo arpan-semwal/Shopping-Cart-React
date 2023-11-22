a4f4985f885adc318fa7cbe640471facd988f682
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-11-22 13:48:34 +0530

    style.css

--- shopping-cart/src/components/Header.jsx
+++ shopping-cart/src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { AiFillDelete } from "react-icons/ai";
+import React from 'react';
+import { AiFillDelete } from 'react-icons/ai';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
@@ -7,85 +8,76 @@ import { BsFillCartFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import { CartState } from './contextApi/Context';
 import Badge from 'react-bootstrap/Badge';
-import Button from "react-bootstrap/esm/Button";
+import Button from 'react-bootstrap/Button';
 
 const Header = () => {
-  const { state: { cart } , dispatch  , productDispatch , searchQuery} = CartState();
+  const { state: { cart }, dispatch, productDispatch, searchQuery } = CartState();
 
   const removeFromCart = (prod) => {
     dispatch({
-      type: "REMOVE_FROM_CART",
+      type: 'REMOVE_FROM_CART',
       payload: prod,
     });
   };
 
   return (
     <div>
-      <Navbar bg='dark' variant='dark' alignRight style={{height:80}}>
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
         <Container>
           <Navbar.Brand>
-            <Link to='/'>Shopping Cart</Link>
+            <Link to="/">Shopping Cart</Link>
           </Navbar.Brand>
-          <Navbar.Text className='search'>
-            <Form.Control
-              placeholder="Search a product"
-              type="text"
-              style={{ width: 500 }}
-              className='m-auto'
-              aria-describedby="basic-addon1"
-              onChange={(e) => {
-                productDispatch({
-                  type:"FILTER_BY_SEARCH",
-                  payload:e.target.value,
-                })
-              }}
-            />
-          </Navbar.Text>
-          
-          <Dropdown align="right">
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              <BsFillCartFill color='white' fontSize="20fpx" />
-              <Badge>{cart.length}</Badge>
-            </Dropdown.Toggle>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Form className="mx-auto my-2 my-lg-0">
+              <Form.Control
+                placeholder="Search a product"
+                type="text"
+                style={{ width: '100%' }}
+                onChange={(e) => {
+                  productDispatch({
+                    type: 'FILTER_BY_SEARCH',
+                    payload: e.target.value,
+                  });
+                }}
+              />
+            </Form>
+            <Dropdown align="right">
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                <BsFillCartFill color="white" fontSize="20fpx" />
+                <Badge>{cart.length}</Badge>
+              </Dropdown.Toggle>
 
-            <Dropdown.Menu style={{}} >
-              {cart.length > 0 ? (
-                cart.map((prod) => (
-                  <span className='cartItem' key={prod.id}>
-                    <img
-                      src={prod.image}
-                      alt={prod.name}
-                      className='cartItemImg'
-                    />
-
-                    <div className='cartItemDetail'>
-                      <span>{prod.name}</span>
-                      <span>₹{prod.price.split(".")[0]}</span>
-                    </div>
-                    <AiFillDelete  
-                      fontSize="20px"
-                      style={{cursor:"pointer"}}
-                        onClick={() => removeFromCart(prod)} 
-                    />
-
-                  <Link to="/cart">
-                    <Button style={{width:"95%" , margin:" 0 10px"}}>
-                      Go To Cart
-                    </Button>
-                  </Link>
-
-
-                  </span>
-                ))) : (
-                <span style={{ padding: 10 }}>Cart is Empty!</span>
-              )}
-            </Dropdown.Menu>
-          </Dropdown>
-
+              <Dropdown.Menu>
+                {cart.length > 0 ? (
+                  cart.map((prod) => (
+                    <span className="cartItem" key={prod.id}>
+                      <img src={prod.image} alt={prod.name} className="cartItemImg" />
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>₹{prod.price.split('.')[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => removeFromCart(prod)}
+                      />
+                      <Link to="/cart">
+                        <Button style={{ width: '95%', margin: ' 0 10px' }}>Go To Cart</Button>
+                      </Link>
+                    </span>
+                  ))
+                ) : (
+                  <span style={{ padding: 10 }}>Cart is Empty!</span>
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
+      {/* Your sidebar component here */}
     </div>
-  )
-}
+  );
+};
 
 export default Header;
